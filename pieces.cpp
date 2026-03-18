0ebf3bc9f0176d576a9968534cf0bda40c7157b3
Nicolas <nicolasanacona161@gmail.com>
2026-03-17 23:44:14 -0500

    Add pieces header and implementation

---
+++ pieces.cpp
@@ -0,0 +1,108 @@
+#include "pieces.h"
+#include <random>
+
+using namespace std;
+/*
+#define MAX_TAMANO_PIEZA 4
+#define PIEZA_I 0
+#define PIEZA_O 1
+#define PIEZA_T 2
+#define PIEZA_S 3
+#define PIEZA_Z 4
+#define PIEZA_J 5
+#define PIEZA_L 6
+#define NUMERO_PIEZAS 7
+ */
+
+void obtener_pieza_inicial(int tipo, unsigned short forma[4], int* ancho, int* alto) {
+    for (int i = 0; i < 4; i++) {
+        forma[i] = 0;
+    }
+
+    switch (tipo) {
+    case 0:
+        forma[0] = 0b1111000000000000;
+        *ancho = 4;
+        *alto = 1;
+        break;
+    case 1:
+        forma[0] = 0b1100000000000000;
+        forma[1] = 0b1100000000000000;
+        *ancho = 2;
+        *alto = 2;
+        break;
+    case 2:
+        forma[0] = 0b1110000000000000;
+        forma[1] = 0b0100000000000000;
+        *ancho = 3;
+        *alto = 2;
+        break;
+    case 3:
+        forma[0] = 0b0110000000000000;
+        forma[1] = 0b1100000000000000;
+        *ancho = 3;
+        *alto = 2;
+        break;
+    case 4:
+        forma[0] = 0b1100000000000000;
+        forma[1] = 0b0110000000000000;
+        *ancho = 3;
+        *alto = 2;
+        break;
+    case 5:
+        forma[0] = 0b1000000000000000;
+        forma[1] = 0b1110000000000000;
+        *ancho = 3;
+        *alto = 2;
+        break;
+    case 6:
+        forma[0] = 0b0010000000000000;
+        forma[1] = 0b1110000000000000;
+        *ancho = 3;
+        *alto = 2;
+        break;
+    default:
+        *ancho = 0;
+        *alto = 0;
+        break;
+    }
+}
+
+void rotar_forma_pieza(const unsigned short forma_entrada[4], int ancho_entrada, int alto_entrada,
+                       unsigned short forma_salida[4], int* ancho_salida, int* alto_salida) {
+    for (int i = 0; i < 4; i++) {
+        forma_salida[i] = 0;
+    }
+
+    int bits[4][4] = {{0}};
+
+    for (int fila = 0; fila < alto_entrada; fila++) {
+        unsigned short fila_actual = forma_entrada[fila];
+        for (int columna = 0; columna < ancho_entrada; columna++) {
+            if (fila_actual & (1 << (15 - columna))) {
+                bits[fila][columna] = 1;
+            }
+        }
+    }
+
+    for (int fila = 0; fila < alto_entrada; fila++) {
+        for (int columna = 0; columna < ancho_entrada; columna++) {
+            if (bits[fila][columna]) {
+                int nueva_fila = columna;
+                int nueva_columna = alto_entrada - 1 - fila;
+                forma_salida[nueva_fila] |= (1 << (15 - nueva_columna));
+            }
+        }
+    }
+
+    *ancho_salida = alto_entrada;
+    *alto_salida = ancho_entrada;
+}
+
+int pieza_aleatoria() {
+    static random_device dispositivo_aleatorio;
+    static mt19937 generador(dispositivo_aleatorio());
+    static uniform_int_distribution<int> distribucion(0, 6);
+
+    return distribucion(generador);
+}
